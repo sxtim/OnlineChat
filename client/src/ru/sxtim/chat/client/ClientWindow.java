9ffa6d365f4b7f0af09a205b0789252bbcfdce06
sxtim <sxtimx@gmail.com>
2021-11-18 22:11:00 +0300

    initial commit

---
+++ client/src/ru/sxtim/chat/client/ClientWindow.java
@@ -0,0 +1,8 @@
+package ru.sxtim.chat.client;
+
+public class ClientWindow {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ server/src/ru/sxtim/chat/server/ChatServer.java
@@ -0,0 +1,7 @@
+package ru.sxtim.chat.server;
+
+public class ChatServer {
+    public static void main(String[] args) {
+
+    }
+}
